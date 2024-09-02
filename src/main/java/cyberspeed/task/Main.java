7e620cc6c2517495e944edcb9ea21faa2f292f9c
Abdurashid Vakhobov <avcoding01@gmail.com>
2024-09-02 15:10:24 +0500

    Implements README and building jar file.

--- src/main/java/cyberspeed/task/Main.java
+++ src/main/java/cyberspeed/task/Main.java
@@ -13,31 +13,24 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String[] temp = new String[4];
-
-        temp[0] = "--config";
-        temp[1] = "./docs/config.json";
-        temp[2] = "--betting-amount";
-        temp[3] = "100";
-
         Inputs inputs = new Inputs();
 
-        inputs.initialize(temp);
+        if (inputs.initialize(args)) {
+            ObjectMapper mapper = JsonMapper.builder()
+                    .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+                    .build();
 
-        ObjectMapper mapper = JsonMapper.builder()
-                .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
-                .build();
+            try {
+                Config config = mapper.readValue(new File(inputs.path()), Config.class);
+                config.rows(config.rows() - 1);
+                config.columns(config.columns() - 1);
 
-        try {
-            Config config = mapper.readValue(new File(inputs.path()), Config.class);
-            config.rows(config.rows() - 1);
-            config.columns(config.columns() - 1);
+                ScratchBoard result = new Scratch(config).scratch(inputs.bettingAmount());
 
-            ScratchBoard result = new Scratch(config).scratch(inputs.bettingAmount());
-
-            mapper.writerWithDefaultPrettyPrinter().writeValue(System.out, result);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
+                mapper.writerWithDefaultPrettyPrinter().writeValue(System.out, result);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
         }
     }
 }
